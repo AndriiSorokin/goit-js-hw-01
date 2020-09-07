640674a139aefb8f6b8576524d27cf8056a11d51
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2020-09-07 13:48:50 +0300

    hw-1 is done

--- task-5.js
+++ task-5.js
@@ -1,35 +1,31 @@
-const china = 'китай'
-const chili = 'чили'
-const australia = 'австралия'
-const india = 'индия'
-const jamaica = 'ямайка'
+const china = "китай";
+const chili = "чили";
+const australia = "австралия";
+const india = "индия";
+const jamaica = "ямайка";
 
-const userAnswer = prompt('Введите название страны доставки')
+const userAnswer = prompt("Введите название страны доставки");
 switch (userAnswer.toLowerCase()) {
-    case china :
-        console.log(`Доставка в ${userAnswer} будет стоить 100 кредитов`)
-        break;
+  case china:
+    console.log(`Доставка в ${userAnswer} будет стоить 100 кредитов`);
+    break;
 
-    case chili :
-            console.log(`Доставка в ${userAnswer} будет стоить 250 кредитов`);
-            break;
+  case chili:
+    console.log(`Доставка в ${userAnswer} будет стоить 250 кредитов`);
+    break;
 
-            case australia :
-                console.log(`Доставка в ${userAnswer} будет стоить 170 кредитов`)
-                break;
+  case australia:
+    console.log(`Доставка в ${userAnswer} будет стоить 170 кредитов`);
+    break;
 
-                case india :
-                    console.log(`Доставка в ${userAnswer} будет стоить 80 кредитов`)
-                    break;
+  case india:
+    console.log(`Доставка в ${userAnswer} будет стоить 80 кредитов`);
+    break;
 
-                    case jamaica :
-                        console.log(`Доставка в ${userAnswer} будет стоить 120 кредитов`)
-                        break;
-                        
-                  default:
-                      alert('В вашей стране нет доставки, досвидос')      
+  case jamaica:
+    console.log(`Доставка в ${userAnswer} будет стоить 120 кредитов`);
+    break;
+
+  default:
+    alert("В вашей стране нет доставки, досвидос");
 }
-
-
-
-
